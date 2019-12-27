e417a246c77391a10451e58d2aa0c3d788af38c2
qilei <lei.qi@duobei.com>
2019-12-27 11:28:39 +0800

    fix: 拆离data为单独文件

---
+++ data.js
@@ -0,0 +1,24 @@
+window.dataJson = {
+  'paas-authinfo': {
+    label: 'paas authinfo的配置',
+    url: '/channel/authinfo',
+    data: {
+      'appId/partner': '',
+      'timestamp': '',
+      'uid': '',
+      'nickname': '',
+      'channelId': ''
+    }
+  },
+  'saas-authinfo': {
+    label: 'saas authinfo的配置',
+    url: '/api/v3/room/authinfo',
+    data: {
+      'partner': '',
+      'timestamp': '',
+      'uid': '',
+      'nickname': '',
+      'roomId': ''
+    }
+  }
+}
