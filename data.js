bc7b7c5cdee5d2f82ecf05326c6e5cb703b78244
qilei <lei.qi@duobei.com>
2019-12-30 17:22:51 +0800

    fix: 改变data文件内容

--- data.js
+++ data.js
@@ -10,8 +10,8 @@ window.dataJson = {
       'channelId': ''
     }
   },
-  'saas-authinfo': {
-    label: 'saas authinfo的配置',
+  'saas-authinfo-v3': {
+    label: 'saas v3 authinfo的配置',
     url: '/api/v3/room/authinfo',
     data: {
       'partner': '',
@@ -20,5 +20,44 @@ window.dataJson = {
       'nickname': '',
       'roomId': ''
     }
-  }
+  },
+  'saas-authinfo-v4': {
+    label: 'saas v4 authinfo的配置',
+    url: '/api/v4/room/authinfo',
+    data: {
+      'partner': '',
+      'timestamp': '',
+      'uid': '',
+      'nickname': '',
+      'roomId': ''
+    }
+  },
+  'admin-authinfo': {
+    label: 'admin的 authinfo的配置',
+    url: '/api/admin/room/authinfo',
+    data: {
+      'partner': '',
+      'timestamp': '',
+      'zhibo': '',
+      'roomId': ''
+    }
+  },
+  'saas-package-status': {
+    label: 'saas的离线打包状态',
+    url: '/api/v3/room/package/status',
+    data: {
+      'partner': '',
+      'timestamp': '',
+      'roomId': ''
+    }
+  },
+  'saas-package-push': {
+    label: 'saas的离线打包执行接口',
+    url: '/api/v3/room/package/push',
+    data: {
+      'partner': '',
+      'timestamp': '',
+      'roomId': ''
+    }
+  },
 }
