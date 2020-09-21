dab769e81846299b3c85aafd796d5d95b5b4cf94
qilei <lei.qi@duobei.com>
2020-09-21 12:33:07 +0800

    fix: 去掉appid必填选项

--- data.js
+++ data.js
@@ -3,114 +3,114 @@ window.dataJson = {
     label: 'paas authinfo的配置',
     url: '/channel/authinfo',
     data: {
-      'body': '',
       'uid': '',
       'nickname': '',
-      'channelId': ''
+      'channelId': '',
+      'body': '',
     }
   },
   'saas-authinfo-v3': {
     label: 'saas v3 authinfo的配置',
     url: '/api/v3/room/authinfo',
     data: {
-      'body': '',
       'uid': '',
       'nickname': '',
-      'roomId': ''
+      'roomId': '',
+      'body': '',
     }
   },
   'saas-authinfo-v4': {
     label: 'saas v4 authinfo的配置',
     url: '/api/v4/room/authinfo',
     data: {
-      'body': '',
       'uid': '',
       'nickname': '',
-      'roomId': ''
+      'roomId': '',
+      'body': '',
     }
   },
   'admin-authinfo': {
     label: 'admin的 authinfo的配置',
     url: '/api/admin/room/authinfo',
     data: {
-      'body': '',
       'zhibo': '',
-      'roomId': ''
+      'roomId': '',
+      'body': '',
     }
   },
   'saas-package-status': {
     label: 'saas的离线打包状态',
     url: '/api/v3/room/package/status',
     data: {
+      'roomId': '',
       'body': '',
-      'roomId': ''
     }
   },
   'saas-package-push': {
     label: 'saas的离线打包执行接口',
     url: '/api/v3/room/package/push',
     data: {
+      'roomId': '',
       'body': '',
-      'roomId': ''
     }
   },
   'saas-v3-创建课程': {
     label: 'saas-v3-创建课程',
     url: '/api/v3/room/create',
     data: {
-      'body': '',
       'title': '',
       'roomType': '',
       'startTime': '',
       'duration': '',
-      'clientType': 0
+      'clientType': 0,
+      'body': '',
     }
   },
   'saas-v4-创建课程': {
     label: 'saas-v4-创建课程',
     url: '/api/v4/room/create',
     data: {
-      'body': '',
       'title': '',
       'roomType': '',
       'startTime': '',
       'duration': '',
       'video': 1,
-      'clientType': 0
+      'clientType': 0,
+      'body': '',
     }
   },
   'saas-直播教室统计': {
     label: 'saas-直播教室统计',
     url: '/api/v3/statistics/zhibo/room',
     data: {
+      'roomId': '',
       'body': '',
-      'roomId': ''
     }
   },
   'saas-直播用户统计': {
     label: 'saas-直播用户统计',
     url: '/api/v3/statistics/zhibo/user',
     data: {
-      'body': '',
       'roomId': '',
-      'uid': ''
+      'uid': '',
+      'body': '',
     }
   },
   'saas-回放教室统计': {
     label: 'saas-回放教室统计',
     url: '/api/v3/statistics/statistics/room',
     data: {
+      'roomId': '',
       'body': '',
-      'roomId': ''
     }
   },
   'saas-回放用户统计': {
     label: 'saas-回放用户统计',
     url: '/api/v3/statistics/playback/user',
     data: {
-      'body': '',
       'roomId': '',
-      'uid': ''
+      'uid': '',
+      'body': '',
     }
   },
 }
